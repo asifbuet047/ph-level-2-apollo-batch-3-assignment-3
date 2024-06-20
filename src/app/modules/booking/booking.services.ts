38215d8e5659f9540b4089a0354091fc82327b77
asifbuet047@gmail.com <asifbuet047@gmail.com>
2024-06-20 18:32:44 +0600

    update gitignore

--- src/app/modules/booking/booking.services.ts
+++ src/app/modules/booking/booking.services.ts
@@ -1,8 +1,6 @@
 import { TBooking } from "./booking.interface";
 import { BookingModel } from "./booking.model";
-import bcrypt from "bcrypt";
-
-const hash = "$2b$08$MSdoojg34UohwFWe3ZvaBuv4Hlc4xkDDfZ.rwM8gwE8a61i5t59qe";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const createBookingIntoDB = async (booking: TBooking) => {
   const result = await BookingModel.create(booking);
