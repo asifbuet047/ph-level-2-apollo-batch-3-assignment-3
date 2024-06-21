0f2a2389dba0f0ac80f1632baed049861a49bbe3
asifbuet047@gmail.com <asifbuet047@gmail.com>
2024-06-21 18:45:26 +0600

    A working directory where mongoose transaction and rollback feature can be seen. When booking a bike after booking doc is created then isVailable status of bike doc is chnaged but if this operation failed previous booking doc is also rollbacked

--- src/app/modules/booking/booking.services.ts
+++ src/app/modules/booking/booking.services.ts
@@ -22,63 +22,62 @@ const createBookingIntoDB = async (
     console.log("Start transaction");
 
     const bikeId = booking.bikeId;
-    const isBikeExits = await BikeServices.getSingleBikeFromDB(
-      bikeId.toString()
-    );
-    if (isBikeExits) {
-      if (isBikeExits.isAvailable) {
-        const user =
-          await UserServices.getSingleUserFromDbExcludingHashedPassword(
-            userEmail
-          );
+    // const isBikeExits = await BikeServices.getSingleBikeFromDB(
+    //   bikeId.toString()
+    // );
+    // if (isBikeExits) {
+    // if (isBikeExits.isAvailable) {
+    const user =
+      await UserServices.getSingleUserFromDbExcludingHashedPassword(userEmail);
 
-        if (user) {
-          const booking: TBooking = {
-            userId: user._id,
-            bikeId: bikeId,
-            startTime: new Date(),
-          };
-          const rent = await BookingModel.create([booking], {
+    if (user) {
+      const booking: TBooking = {
+        userId: user._id,
+        bikeId: bikeId,
+        startTime: new Date(),
+      };
+      const rent = await BookingModel.create([booking], {
+        session: mongooseTransactionSession,
+      });
+      if (rent.length) {
+        const isChangeAvailability = await BikeModel.findByIdAndUpdate(
+          [bikeId],
+          {
+            isAvailable: false,
+          },
+          {
             session: mongooseTransactionSession,
-          });
-          if (rent.length) {
-            const isChangeAvailability = await BikeModel.findByIdAndUpdate(
-              [bikeId],
-              {
-                isAvailable: false,
-              },
-              {
-                session: mongooseTransactionSession,
-                new: true,
-              }
-            );
-            if (!isChangeAvailability?.isAvailable) {
-              await mongooseTransactionSession.commitTransaction();
-              console.log("Abort tranaction after successful operation");
-              await mongooseTransactionSession.endSession();
-              console.log("End session after successful opearation");
-              return rent;
-            } else {
-              throw new DatabaseOperationFailedError(
-                "Bike document write failed into DB",
-                httpStatus.FAILED_DEPENDENCY
-              );
-            }
-          } else {
-            throw new DatabaseOperationFailedError(
-              "Booking document write failed into DB",
-              httpStatus.FAILED_DEPENDENCY
-            );
+            new: true,
           }
+        );
+        console.log(isChangeAvailability);
+        if (isChangeAvailability) {
+          await mongooseTransactionSession.commitTransaction();
+          console.log("Abort tranaction after successful operation");
+          await mongooseTransactionSession.endSession();
+          console.log("End session after successful opearation");
+          return rent[0];
         } else {
-          throw new NoDataFoundError("No user found", httpStatus.NOT_FOUND);
+          throw new DatabaseOperationFailedError(
+            "Bike document write failed into DB",
+            httpStatus.FAILED_DEPENDENCY
+          );
         }
       } else {
-        throw new BikeNotAvailableError();
+        throw new DatabaseOperationFailedError(
+          "Booking document write failed into DB",
+          httpStatus.FAILED_DEPENDENCY
+        );
       }
     } else {
-      throw new NoBikeFoundError();
+      throw new NoDataFoundError("No user found", httpStatus.NOT_FOUND);
     }
+    // } else {
+    //   throw new BikeNotAvailableError();
+    // }
+    // } else {
+    //   throw new NoBikeFoundError();
+    // }
   } catch (error) {
     await mongooseTransactionSession.abortTransaction();
     console.log("Abort transaction after error");
@@ -91,6 +90,19 @@ const createBookingIntoDB = async (
 const getAllBookingFromDB = async () => {
   const result = await BookingModel.find();
   return result;
+};
+
+const updateBookingIntoDB = async (booingId: string) => {
+  const mongooseTransactionSession = await mongoose.startSession();
+  try {
+    mongooseTransactionSession.startTransaction();
+  } catch (error) {
+    await mongooseTransactionSession.abortTransaction();
+    console.log("Abort transaction after error");
+    await mongooseTransactionSession.endSession();
+    console.log("End session after error");
+    throw error;
+  }
 };
 
 export const BookingServices = {
